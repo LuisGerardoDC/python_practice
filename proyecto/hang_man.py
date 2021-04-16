f2a6d90c7f763a2068829d22aac6084085d4e584
Luis Hernandez <luisgerarhern@outlook.com>
2021-04-16 14:11:08 -0500

    correccion de carteles y hangman

--- proyecto/hang_man.py
+++ proyecto/hang_man.py
@@ -41,37 +41,38 @@ def join_results(new_result, old_result):
         else:
             joined_result.append('_')
     return joined_result
-    
+
 def play(chosen_word):
     attempts = 0
+    asserts = 1
     word_list = list(chosen_word)
+    len_word = len(word_list)
     chanses=6
     result = ['_' for letter in chosen_word]
     result[0] = word_list[0]
-    print(TITLE)
-    print(f'intentos Restantes: {chanses}')
-    print(HANG_MAN[0])
-    print(result)
 
-    while(chanses > attempts):
+    while(chanses > attempts and asserts<len_word):
         try:
-            letter = get_one_letter()
-            temporal = compare_attempt(letter, word_list)
-            if temporal:
-                result = join_results(temporal,result)
-            else:
-                attempts += 1
-                print('has perdido un intento')
+            print(TITLE)
             print(f'intentos Restantes: {chanses-attempts}')
             print(HANG_MAN[attempts])
             print(result)
 
+            letter = get_one_letter()
+            temporal = compare_attempt(letter, word_list)
+            if temporal:
+                result = join_results(temporal,result)
+                asserts +=1
+            else:
+                attempts += 1
+
         except ValueError as ve:
-            print(result)
+            attempts +=1
+            system('clear')
             print(ve)
 
     system('clear')
-    if(chosen_word == ''.join(result)):
+    if(asserts == len_word):
         print(YOU_WON)
     else:
         print(HANG_MAN[len(HANG_MAN)-1])
@@ -81,8 +82,13 @@ def play(chosen_word):
 
 def run():
     words = read_file()
-    chosen_word = chose_word(words)
-    play(chosen_word)
-
+    option = '1'
+    while(option == '1'):
+        chosen_word = chose_word(words)
+        play(chosen_word)
+        option= input('''
+        Jugar de nuevo? 1.-Si 0.-No
+        ''')
+        system('clear')
 if __name__=="__main__":
    run()
